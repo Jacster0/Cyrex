a0053542b6674faf53491fff28e3e4fa518ca42a
_Jacce_ <jacob.ogren2@gmail.com>
2020-10-07 21:09:31 +0200

    define the NOMINMAX macro inside CrxWindow

--- Cyrex/Platform/Windows/CrxWindow.h
+++ Cyrex/Platform/Windows/CrxWindow.h
@@ -1,5 +1,15 @@
 #pragma once
 #define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
 #include <Windows.h>
+
+//The following code is redundant when NOMINMAX is defined
+//However I will keep it as some sort of backup in case some sorcerer 
+//decides to delete the NOMINMAX macro 
+#ifdef min
 #undef min
+#endif
+
+#ifdef max
 #undef max
+#endif
