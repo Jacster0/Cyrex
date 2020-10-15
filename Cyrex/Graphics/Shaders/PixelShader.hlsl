eb927637fb166238a8edb9f3a7b0d780a4db2ddd
_Jacce_ <jacob.ogren2@gmail.com>
2020-10-16 00:25:09 +0200

    The shaders didn't wanna compile because I made a lot of typos which Visual Studio did not want to tell me about

--- Cyrex/Graphics/Shaders/PixelShader.hlsl
+++ Cyrex/Graphics/Shaders/PixelShader.hlsl
@@ -1,8 +1,9 @@
-struct PixelShaderInput {
-	float4 Color : COLOR;
+struct PixelShaderInput
+{
+    float4 Color : COLOR;
 };
 
-float4 main(PixelShaderInput IN) : SV_TARGET
+float4 main(PixelShaderInput IN) : SV_Target
 {
-	return IN.Color;
+    return IN.Color;
 }
--- Cyrex/Graphics/Shaders/VertexShader.hlsl
+++ Cyrex/Graphics/Shaders/VertexShader.hlsl
@@ -1,24 +1,28 @@
-struct ModelViewProjection {
-	matrix MVP;
+struct ModelViewProjection
+{
+    matrix MVP;
 };
 
 ConstantBuffer<ModelViewProjection> ModelViewProjectionCB : register(b0);
 
-struct VertexposColor {
-	float3 Position : POSITION;
-	float3 Color    : COLOR;
-};
-
-struct VertexShaderoutput {
-	float4 Color    : COLOR;
-	float4 Position : SV_POSITION;
-};
-
-VertexShaderOutput main(VertexPosColor IN) : SV_POSITION
+struct VertexPosColor
 {
-	VertexShaderOutput OUT;
+    float3 Position : POSITION;
+    float3 Color    : COLOR;
+};
 
-    OUT.Position = mul(ModelViewProjection.MVP, float4(IN.POSITION, 1.0f));
-	OUT.Color = float4(IN.Color, 1.0f);
-	return OUT;
+struct VertexShaderOutput
+{
+    float4 Color    : COLOR;
+    float4 Position : SV_Position;
+};
+
+VertexShaderOutput main(VertexPosColor IN)
+{
+    VertexShaderOutput OUT;
+
+    OUT.Position = mul(ModelViewProjectionCB.MVP, float4(IN.Position, 1.0f));
+    OUT.Color = float4(IN.Color, 1.0f);
+
+    return OUT;
 }
