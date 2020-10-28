7b3fb6bea7623a858043477f8f414123e4f95051
_Jacce_ <jacob.ogren2@gmail.com>
2020-10-28 10:12:24 +0100

    StringUtils should be in the Cyrex namespace

--- Cyrex/Core/Utils/StringUtils.h
+++ Cyrex/Core/Utils/StringUtils.h
@@ -2,14 +2,16 @@
 #include <string>
 #include <iomanip>
 
-std::string ToNarrow(const std::wstring& wide) {
-    char narrow[512];
-    wcstombs_s(nullptr, narrow, wide.c_str(), _TRUNCATE);
-    return narrow;
-}
+namespace Cyrex {
+    std::string ToNarrow(const std::wstring& wide) {
+        char narrow[512];
+        wcstombs_s(nullptr, narrow, wide.c_str(), _TRUNCATE);
+        return narrow;
+    }
 
-std::wstring ToWide(const std::string& narrow) {
-    wchar_t wide[512];
-    mbstowcs_s(nullptr, wide, narrow.c_str(), _TRUNCATE);
-    return wide;
+    std::wstring ToWide(const std::string& narrow) {
+        wchar_t wide[512];
+        mbstowcs_s(nullptr, wide, narrow.c_str(), _TRUNCATE);
+        return wide;
+    }
 }
