2493e8e4104a509f6c748395ab08b41f301888bd
_Jacce_ <jacob.ogren2@gmail.com>
2020-12-02 12:18:17 +0100

    Added a Thread safe queue data structure

---
+++ Cyrex/Core/ThreadSafeQueue.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#include <queue>
+#include <mutex>
+
+template<class T>
+class ThreadSafeQueue {
+public:
+    ThreadSafeQueue();
+    ThreadSafeQueue(const ThreadSafeQueue& rhs);
+
+    void Push(T value);
+    bool TryPop(T& value);
+    bool Empty() const;
+    size_t Size() const;
+private:
+    std::queue<T> m_queue;
+    mutable std::mutex m_mutex;
+};
+
+template<class T>
+ThreadSafeQueue<T>::ThreadSafeQueue() { }
+
+template<class T>
+ThreadSafeQueue<T>::ThreadSafeQueue(const ThreadSafeQueue& rhs) {
+    std::lock_guard<std::mutex> lock(rhs.m_mutex);
+    m_queue = rhs.m_queue;
+}
+
+template<class T>
+void ThreadSafeQueue<T>::Push(T value) {
+    std::lock_guard<std::mutex> lock(m_Mutex);
+    m_queue.push(std::move(value));
+}
+
+template<class T>
+bool ThreadSafeQueue<T>::TryPop(T& value) {
+    std::lock_guard<std::mutex> lock(m_Mutex);
+    if (m_queue.empty()) {
+        return false;
+    }
+    value = m_queue.front();
+    m_queue.pop();
+
+    return true;;
+}
+
+template<class T>
+bool ThreadSafeQueue<T>::Empty() const {
+    std::lock_guard<std::mutex> lock(m_Mutex);
+    return m_queue.empty();
+}
+
+template<class T>
+size_t ThreadSafeQueue<T>::Size() const {
+    std::lock_guard<std::mutex> lock(m_Mutex);
+    return m_queue.size();
+}
