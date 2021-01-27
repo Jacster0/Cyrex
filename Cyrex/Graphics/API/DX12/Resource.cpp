e03c480f720873fc75eabb595a686e504faf4720
_Jacce_ <jacob.ogren2@gmail.com>
2021-01-27 16:00:43 +0100

    Cleaned up the Resource class

--- Cyrex/Graphics/API/DX12/Resource.cpp
+++ Cyrex/Graphics/API/DX12/Resource.cpp
@@ -1,91 +1,8 @@
 #include "Resource.h"
 #include "d3dx12.h"
-#include "Core/Application.h"
 #include "DXException.h"
 #include "ResourceStateTracker.h"
-
-Cyrex::Resource::Resource(const std::wstring& name)
-    :
-    m_resourceName(name),
-    m_formatSupport({})
-{}
-
-Cyrex::Resource::Resource(
-    const D3D12_RESOURCE_DESC& resourceDesc, 
-    const D3D12_CLEAR_VALUE* clearValue, 
-    const std::wstring& name)
-{
-    if (clearValue) {
-        m_d3d12ClearValue = std::make_unique<D3D12_CLEAR_VALUE>(*clearValue);
-    }
-
-    const auto device = Application::Get().GetDevice();
-
-    ThrowIfFailed(device->CreateCommittedResource(
-        &CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT),
-        D3D12_HEAP_FLAG_NONE,
-        &resourceDesc,
-        D3D12_RESOURCE_STATE_COMMON,
-        m_d3d12ClearValue.get(),
-        IID_PPV_ARGS(&m_d3d12Resource)));
-
-    ResourceStateTracker::AddGlobalResourceState(m_d3d12Resource.Get(), D3D12_RESOURCE_STATE_COMMON);
-    CheckFeatureSupport();
-    SetName(name);
-}
-
-Cyrex::Resource::Resource(Microsoft::WRL::ComPtr<ID3D12Resource> resource, const std::wstring& name)
-    :
-    m_d3d12Resource(resource),
-    m_formatSupport({})
-{
-    CheckFeatureSupport();
-    SetName(name);
-}
-
-Cyrex::Resource::Resource(const Resource& rhs)
-    :
-    m_d3d12Resource(rhs.m_d3d12Resource),
-    m_formatSupport(rhs.m_formatSupport),
-    m_resourceName(rhs.m_resourceName),
-    m_d3d12ClearValue(std::make_unique<D3D12_CLEAR_VALUE>(*rhs.m_d3d12ClearValue))
-{}
-
-Cyrex::Resource::Resource(Resource&& rhs) noexcept
-    :
-
-    m_d3d12Resource(std::move(rhs.m_d3d12Resource)),
-    m_formatSupport(rhs.m_formatSupport),
-    m_resourceName(std::move(rhs.m_resourceName)),
-    m_d3d12ClearValue(std::move(m_d3d12ClearValue))
-{}
-
-Cyrex::Resource& Cyrex::Resource::operator=(const Resource& rhs) {
-    if (this != &rhs) {
-        m_d3d12Resource = rhs.m_d3d12Resource;
-        m_formatSupport = rhs.m_formatSupport;
-        m_resourceName  = rhs.m_resourceName;
-
-        if (rhs.m_d3d12ClearValue) {
-            m_d3d12ClearValue = std::make_unique<D3D12_CLEAR_VALUE>(*rhs.m_d3d12ClearValue);
-        }
-    }
-
-    return *this;
-}
-
-Cyrex::Resource& Cyrex::Resource::operator=(Resource&& rhs) noexcept {
-    if (this != &rhs) {
-        m_d3d12Resource   = std::move(rhs.m_d3d12Resource);
-        m_formatSupport   = rhs.m_formatSupport;
-        m_resourceName    = std::move(rhs.m_resourceName);
-        m_d3d12ClearValue = std::move(rhs.m_d3d12ClearValue);
-
-        rhs.Reset();
-    }
-
-    return *this;
-}
+#include "Device.h"
 
 D3D12_RESOURCE_DESC Cyrex::Resource::GetD3D12ResourceDesc() const noexcept {
     D3D12_RESOURCE_DESC resDesc = {};
@@ -95,30 +12,6 @@ D3D12_RESOURCE_DESC Cyrex::Resource::GetD3D12ResourceDesc() const noexcept {
     }
 
     return resDesc;
-}
-
-void Cyrex::Resource::SetD3D12Resource(
-    Microsoft::WRL::ComPtr<ID3D12Resource> d3d12Resource, 
-    const D3D12_CLEAR_VALUE* clearValue)
-{
-    m_d3d12Resource = d3d12Resource;
-
-    if (m_d3d12ClearValue) {
-        m_d3d12ClearValue = std::make_unique<D3D12_CLEAR_VALUE>(*clearValue);
-    }
-    else {
-        m_d3d12ClearValue.reset();
-    }
-
-    CheckFeatureSupport();
-    SetName(m_resourceName);
-}
-
-void Cyrex::Resource::Reset() {
-    m_d3d12Resource.Reset();
-    m_formatSupport = {};
-    m_d3d12ClearValue.reset();
-    m_resourceName.clear();
 }
 
 void Cyrex::Resource::SetName(const std::wstring& name) {
@@ -137,19 +30,53 @@ bool Cyrex::Resource::CheckFormatSupport(D3D12_FORMAT_SUPPORT2 formatSupport) co
     return (m_formatSupport.Support2 & formatSupport) != 0;
 }
 
+Cyrex::Resource::Resource(Device& device, const D3D12_RESOURCE_DESC& resourceDesc, const D3D12_CLEAR_VALUE* clearValue)
+    :
+    m_device(device)
+{
+    const auto d3d12Device = m_device.GetD3D12Device();
+
+    if (clearValue) {
+        m_d3d12ClearValue = std::make_unique<D3D12_CLEAR_VALUE>(*clearValue);
+    }
+
+    auto heapProps = CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT);
+
+    ThrowIfFailed(d3d12Device->CreateCommittedResource(
+        &heapProps,
+        D3D12_HEAP_FLAG_NONE,
+        &resourceDesc,
+        D3D12_RESOURCE_STATE_COMMON,
+        m_d3d12ClearValue.get(),
+        IID_PPV_ARGS(&m_d3d12Resource)));
+
+    ResourceStateTracker::AddGlobalResourceState(m_d3d12Resource.Get(), D3D12_RESOURCE_STATE_COMMON);
+    CheckFeatureSupport();
+}
+
+Cyrex::Resource::Resource(
+    Device& device, 
+    Microsoft::WRL::ComPtr<ID3D12Resource> resource, 
+    const D3D12_CLEAR_VALUE* clearValue)
+    :
+    m_device(device),
+    m_d3d12Resource(resource)
+
+{
+    if (clearValue) {
+        m_d3d12ClearValue = std::make_unique<D3D12_CLEAR_VALUE>(*clearValue);
+    }
+    CheckFeatureSupport();
+}
+
 void Cyrex::Resource::CheckFeatureSupport() {
-    if (m_d3d12Resource) {
-        const auto desc   = m_d3d12Resource->GetDesc();
-        const auto device = Application::Get().GetDevice();
+    const auto device = m_device.GetD3D12Device();
 
-        m_formatSupport.Format = desc.Format;
+    const auto desc = m_d3d12Resource->GetDesc();
+    m_formatSupport.Format = desc.Format;
 
-        ThrowIfFailed(device->CheckFeatureSupport(
-            D3D12_FEATURE_FORMAT_SUPPORT,
-            &m_formatSupport,
-            sizeof(D3D12_FEATURE_DATA_FORMAT_SUPPORT)));
-    }
-    else {
-        m_formatSupport = {};
-    }
+    ThrowIfFailed(device->CheckFeatureSupport(
+        D3D12_FEATURE_FORMAT_SUPPORT,
+        &m_formatSupport,
+        sizeof(D3D12_FEATURE_DATA_FORMAT_SUPPORT)));
 }
--- Cyrex/Graphics/API/DX12/Resource.h
+++ Cyrex/Graphics/API/DX12/Resource.h
@@ -5,41 +5,24 @@
 #include <memory>
 
 namespace Cyrex {
+    class Device;
     class Resource {
     public:
-        explicit Resource(const std::wstring& name = L"");
-        explicit Resource(const D3D12_RESOURCE_DESC& resourceDesc,
-            const D3D12_CLEAR_VALUE* clearValue = nullptr,
-            const std::wstring& name = L"");
-        explicit Resource(Microsoft::WRL::ComPtr<ID3D12Resource> resource, const std::wstring& name = L"");
-
-        Resource(const Resource& rhs);
-        Resource(Resource&& rhs) noexcept;
-
-        Resource& operator=(const Resource& rhs);
-        Resource& operator=(Resource&& rhs) noexcept;
-        ~Resource() = default;
-    public:
-        bool IsValid() const noexcept { return m_d3d12Resource != nullptr; }
         Microsoft::WRL::ComPtr<ID3D12Resource> GetD3D12Resource() const noexcept { return m_d3d12Resource; }
-
         D3D12_RESOURCE_DESC GetD3D12ResourceDesc() const noexcept;
 
-        virtual void SetD3D12Resource(
-            Microsoft::WRL::ComPtr<ID3D12Resource> d3d12Resource,
-            const D3D12_CLEAR_VALUE* clearValue = nullptr);
-
-        virtual D3D12_CPU_DESCRIPTOR_HANDLE GetShaderResourceView(
-            const D3D12_SHADER_RESOURCE_VIEW_DESC* srvDesc = nullptr) const = 0;
-
-        virtual D3D12_CPU_DESCRIPTOR_HANDLE GetUnorderedAccessView(
-            const D3D12_UNORDERED_ACCESS_VIEW_DESC* uavDesc = nullptr) const = 0;
-        virtual void Reset();
-
         void SetName(const std::wstring& name);
+        const std::wstring& GetName() const { return m_resourceName; }
+
         bool CheckFormatSupport(D3D12_FORMAT_SUPPORT1 formatSupport) const;
         bool CheckFormatSupport(D3D12_FORMAT_SUPPORT2 formatSupport) const;
     protected:
+        Resource(Device& device, const D3D12_RESOURCE_DESC& resourceDesc, const D3D12_CLEAR_VALUE* clearValue = nullptr);
+        Resource(Device& device, Microsoft::WRL::ComPtr<ID3D12Resource> resource, const D3D12_CLEAR_VALUE* clearValue = nullptr);
+        virtual ~Resource() = default;
+    protected:
+        Device& m_device; 
+
         Microsoft::WRL::ComPtr<ID3D12Resource> m_d3d12Resource;
         D3D12_FEATURE_DATA_FORMAT_SUPPORT m_formatSupport;
         std::unique_ptr<D3D12_CLEAR_VALUE> m_d3d12ClearValue;
