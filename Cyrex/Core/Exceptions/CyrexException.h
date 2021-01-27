99cbe8513b2e8e45d4da2f9c3d91c3b533c84a25
_Jacce_ <jacob.ogren2@gmail.com>
2021-01-27 15:33:43 +0100

    Modified CyrexException class

--- Cyrex/Core/Exceptions/CyrexException.h
+++ Cyrex/Core/Exceptions/CyrexException.h
@@ -5,11 +5,13 @@ namespace Cyrex {
     class CyrexException : public std::exception {
     public:
         CyrexException(int line, const std::string file) noexcept;
-        const char* what() const noexcept override;
+        virtual const char* what() const noexcept override;
         int GetLine() const noexcept;
         const std::string& GetFile() const noexcept;
         std::string GetOriginString() const noexcept;
         virtual const std::string GetType() const noexcept;
+    protected:
+        mutable std::string message;
     private:
         int line;
         std::string file;
