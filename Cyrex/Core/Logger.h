d80b6383112aa53aaf6fc9e5d9e4f0a957ec5ebf
_Jacce_ <jacob.ogren2@gmail.com>
2021-02-13 16:26:20 +0100

    Cleaning up

--- Cyrex/Core/Logger.h
+++ Cyrex/Core/Logger.h
@@ -7,17 +7,14 @@ namespace Cyrex {
     enum class Level {crx_default, crx_info, crx_warn, crx_error, crx_critical};
 
     class Logger {
-    private:
-        Logger() = default;
     public:
         static Logger& Get() noexcept;
         void StartUp() const noexcept;
         void ShutDown() noexcept;
-    public:
         void SetOutputStream(OutputStream ostream) noexcept;
         void Reset() noexcept;
         static constexpr auto NewLine() noexcept { return std::endl<char, std::char_traits<char>>; }
-    public:
+
         template<typename... Args>
         void Log(Level lvl, Args&& ...args);
         template<typename... Args>
@@ -27,12 +24,13 @@ namespace Cyrex {
         template<typename... Args>
         void DebugLog(Args&& ...args);
     private:
+        Logger() = default;
+
         void SetOutputStream(std::ostream* stream) noexcept { this->stream = stream; }
         void SetLevel(Level lvl) noexcept;
         void SetLevel(OutputStream ostream, Color clr, std::string_view attribute) noexcept;
-    private:
+
         std::ostream* stream = Console::GetStandardStream();
-    private:
         std::string prefix{};
     };
 
@@ -89,7 +87,7 @@ namespace Cyrex::crxlog {
     }
 
     template<typename ...Args>
-    inline void normal(Args&& ...args) {
+    inline void log(Args&& ...args) {
         Logger::Get().Log(Level::crx_default, args..., Logger::NewLine());
     }
 }
@@ -116,7 +114,7 @@ namespace Cyrex::crxdebuglog {
     }
 
     template<typename ...Args>
-    inline void normal(Args&& ...args) {
+    inline void log(Args&& ...args) {
         Logger::Get().DebugLog(Level::crx_default, args..., Logger::NewLine());
     }
 }
