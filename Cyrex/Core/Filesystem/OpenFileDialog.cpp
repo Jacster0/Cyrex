30afce79ebb7e4d9deddeb56fd415ed878fd1ca7
_Jacce_ <jacob.ogren2@gmail.com>
2021-03-08 00:33:11 +0100

    Added an OpenFileDialog class

---
+++ Cyrex/Core/Filesystem/OpenFileDialog.cpp
@@ -0,0 +1,46 @@
+#include "OpenFileDialog.h"
+
+#include <array>
+
+using namespace Cyrex;
+using namespace Microsoft::WRL;
+
+Cyrex::OpenFileDialog::OpenFileDialog() noexcept {
+    auto hr = CoCreateInstance(CLSID_FileOpenDialog, nullptr, CLSCTX_ALL, IID_PPV_ARGS(&m_openFileDialog));
+
+    m_canShowDialog = SUCCEEDED(hr);
+}
+
+DialogResult OpenFileDialog::Open() noexcept {
+    auto dialogResult = DialogResult::None;
+
+    if (m_canShowDialog) {
+        if (SUCCEEDED(m_openFileDialog->Show(m_ownerHwnd))) {
+            if (GetResult()) {
+                dialogResult = DialogResult::OK;
+            }           
+        }
+    }
+    return dialogResult;
+}
+
+void Cyrex::OpenFileDialog::SetFilterIndex(uint32_t filterIndex) noexcept {
+    m_filterIndex = filterIndex;
+    m_openFileDialog->SetFileTypeIndex(m_filterIndex);
+}
+
+void Cyrex::OpenFileDialog::SetFilter(std::vector<COMDLG_FILTERSPEC>&& fileFilters) noexcept {
+    m_fileFilters = std::move(fileFilters);
+    m_openFileDialog->SetFileTypes(m_fileFilters.size(), m_fileFilters.data());
+}
+
+std::wstring Cyrex::OpenFileDialog::GetDisplayName(_SIGDN sigdnName) const noexcept {
+     PWSTR displayName;
+    if (m_shellItem->GetDisplayName(sigdnName, &displayName) >= 0) {
+        std::wstring res = std::wstring(displayName);
+        CoTaskMemFree(displayName);
+
+        return res;
+    }
+    return std::wstring();
+}
---
+++ Cyrex/Core/Filesystem/OpenFileDialog.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <wrl/client.h>
+#include <ShObjIdl.h> 
+#include <shlwapi.h>
+
+#include <cstdint>
+#include <vector>
+#include <string>
+
+#include "Core/CommonTypes.h"
+
+namespace Cyrex {
+    class OpenFileDialog {
+    public:
+        OpenFileDialog() noexcept;
+
+        [[nodiscard]] DialogResult Open() noexcept;
+        [[nodiscard]] bool GetResult() noexcept { return m_openFileDialog->GetResult(&m_shellItem) >= 0; };
+
+        void SetFilterIndex(uint32_t filterIndex) noexcept;
+        [[nodiscard]] const uint32_t GetFilterIndex() const noexcept { return m_filterIndex; }
+
+        void SetFilter(std::vector<COMDLG_FILTERSPEC>&& fileFilters) noexcept;
+        [[nodiscard]] const std::vector<COMDLG_FILTERSPEC>& GetFilter()  noexcept { return m_fileFilters; }
+
+        [[nodiscard]] std::wstring GetDisplayName(_SIGDN sigdnName) const noexcept;
+        void SetOwnerHWND(HWND owner) noexcept { m_ownerHwnd = owner; }
+    private:
+        Microsoft::WRL::ComPtr<IFileOpenDialog> m_openFileDialog;
+        Microsoft::WRL::ComPtr<IShellItem> m_shellItem;
+        
+        std::vector<COMDLG_FILTERSPEC> m_fileFilters{};
+
+        uint32_t m_filterIndex{};
+        bool m_canShowDialog{ false };
+        HWND m_ownerHwnd{nullptr};
+    };
+}
