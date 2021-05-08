3e34c076486fc9076633f221347c41c7723fec91
Jacster0 <45000710+Jacster0@users.noreply.github.com>
2021-05-08 21:57:02 +0200

    Improved variable names and removed unnecessary comments.

--- Cyrex/Core/InstructionSet/CpuInfo.cpp
+++ Cyrex/Core/InstructionSet/CpuInfo.cpp
@@ -16,26 +16,21 @@ CPUInfo::CPUInfo() {
 }
 
 const uint32_t CPUInfo::GetNumberOfCores() const noexcept {
-    DWORD length{ 0 };
+    DWORD lengthInBytes{ 0 };
     uint32_t numCores{ 0 };
 
     constexpr auto filterLambda = [](const SYSTEM_LOGICAL_PROCESSOR_INFORMATION& elem) {
-        return elem.Relationship == RelationProcessorCore; 
+        return elem.Relationship == RelationProcessorCore;
     };
-
     const auto coreCounterLambda = [&numCores]([[maybe_unused]] const SYSTEM_LOGICAL_PROCESSOR_INFORMATION& elem) {
         numCores++;
     };
 
-    //retrieve the buffer length in bytes
-    GetLogicalProcessorInformation(nullptr, &length);
-
-    //create the buffer
-    std::vector<SYSTEM_LOGICAL_PROCESSOR_INFORMATION> buffer(length / sizeof(SYSTEM_LOGICAL_PROCESSOR_INFORMATION));
+    GetLogicalProcessorInformation(nullptr, &lengthInBytes);
+    std::vector<SYSTEM_LOGICAL_PROCESSOR_INFORMATION> buffer(lengthInBytes / sizeof(SYSTEM_LOGICAL_PROCESSOR_INFORMATION));
 
     //Fill the buffer. If the function succeed we can start counting cores.
-    if (GetLogicalProcessorInformation(buffer.data(), &length)) [[likely]] {
-        //Count the number of physical cores on the users cpu.
+    if (GetLogicalProcessorInformation(buffer.data(), &lengthInBytes)) [[likely]] {
         std::ranges::for_each(buffer | views::filter(filterLambda), coreCounterLambda);
     }
 
